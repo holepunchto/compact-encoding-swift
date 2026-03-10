ce2e6b0c568b9ca061f278106cf5045660e20f8b
Balakrishna Avulapati <ba@bavulapati.com>
2026-03-10 07:52:31 +0530

    remove unnecessary comma
    Signed-off-by: Balakrishna Avulapati <ba@bavulapati.com>

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   ],
   targets: [
     .target(
-      name: "CompactEncoding",
+      name: "CompactEncoding"
     ),
     .testTarget(
       name: "CompactEncodingTests",
